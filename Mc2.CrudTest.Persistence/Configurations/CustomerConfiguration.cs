096cbec9cf148d586fcf6468b271af1d80a240fb
Ramtin-sht <ramtin.shafeghat@gmail.com>
2023-09-24 21:06:23 +0330

    Add more ef configs

--- Mc2.CrudTest.Persistence/Configurations/CustomerConfiguration.cs
+++ Mc2.CrudTest.Persistence/Configurations/CustomerConfiguration.cs
@@ -9,10 +9,16 @@ public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
     {
         builder.HasKey(p => p.Id);
 
-        builder.Property(p => p.FirstName)
-               .IsRequired();
+        builder.Property(p => p.FirstName).HasMaxLength(50).IsRequired();
+        builder.Property(p => p.LastName).HasMaxLength(50).IsRequired();
+        builder.Property(p => p.DateOfBirth).IsRequired();
+        builder.Property(p => p.Email).HasMaxLength(50).IsRequired();
+        builder.Property(p => p.PhoneNumber).HasMaxLength(11).IsRequired();
+        builder.Property(p => p.BankAccountNumber).HasMaxLength(20).IsRequired();
+        builder.Property(p => p.IsDeleted).IsRequired();
 
-        builder.Property(p => p.LastName)
-               .IsRequired();
+        builder.HasIndex(c => new { c.FirstName, c.LastName, c.DateOfBirth }).IsUnique(true);
+        builder.HasIndex(c => c.Email).IsUnique(true);
+
     }
 }
